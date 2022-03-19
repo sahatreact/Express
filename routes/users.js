98c745ac17ecedbc1d694e77344ff1e740db5c63
unknown <sahatreactjs@gmail.com>
2022-03-19 17:33:34 +0700

    Install express dan nodemon

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("Hellow World");
+});
+
+router.get("/me", (req, res, next) => {
+  res.send(" Hi this is me");
+});
+
+module.exports = router;
